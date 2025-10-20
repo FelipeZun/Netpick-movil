24a2b9bb5c679d2bdbe5e55685bd34ec507ec14e
Gaborilincio <ga.mendez@duocuc.cl>
2025-10-20 19:12:23 -0300

    Initial commit: Netpick móvil

---
+++ app/src/main/java/com/example/netpick_movil/model/UsuarioUiState.kt
@@ -0,0 +1,11 @@
+package model
+import model.UsuarioErrores;
+
+data class UsuarioUiState (
+    val nombre: String = "",
+    val correo: String = "",
+    val clave: String = "",
+    val direccion: String = "",
+    val aceptaTerminos: Boolean = false,
+    val errores: UsuarioErrores = UsuarioErrores()
+)
---
+++ app/src/main/java/com/example/netpick_movil/viewmodel/UsuarioViewModel.kt
@@ -0,0 +1,7 @@
+package viewmodel;
+import androidx.lifecycle.ViewModel
+import model.UsuarioErrores;
+
+class UsuarioViewMode : ViewModel() {
+
+}
